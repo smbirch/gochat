30b269ec2e63c3473f12e59cef13c271a0d4da3b
smbirch <spencer.m.birch@gmail.com>
2022-02-11 09:53:57 -0800

    basic .env setup

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module gochat
 go 1.17
 
 require github.com/gorilla/mux v1.8.0
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
 	http.Handle("/", http.FileServer(http.Dir("./public")))
-	log.Println("Server starting at localhost:8000")
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	log.Println("Server starting at localhost :", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
 }
