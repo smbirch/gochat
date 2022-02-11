4b9f647c240d1d238497606461cf7cc9d5c142bf
smbirch <spencer.m.birch@gmail.com>
2022-02-11 11:24:40 -0800

    fixing js issue with buttons

--- public/app.js
+++ public/app.js
@@ -1,22 +1,28 @@
-$(function () {
-  let websocket = newWebSocket("wss://" + window.location.host + "/websocket");
-  let room = $("#chat-text");
+window.addEventListener("DOMContentLoaded", (_) => {
+  let websocket = new WebSocket("ws://" + window.location.host + "/websocket");
+  let room = document.getElementById("chat-text");
+
   websocket.addEventListener("message", function (e) {
     let data = JSON.parse(e.data);
-    let chatContent = `<p><strong>${data.username}</strong>: ${data.text}</p>`;
-    room.append(chatContent);
+    // creating html element
+    let p = document.createElement("p");
+    p.innerHTML = `<strong>${data.username}</strong>: ${data.text}`;
+
+    room.append(p);
     room.scrollTop = room.scrollHeight; // Auto scroll to the bottom
-   });
-  $("#input-form").on("submit", function (event) {
+  });
+
+  let form = document.getElementById("input-form");
+  form.addEventListener("submit", function (event) {
     event.preventDefault();
-    let username = $("#input-username")[0].value;
-    let text = $("#input-text")[0].value;
+    let username = document.getElementById("input-username");
+    let text = document.getElementById("input-text");
     websocket.send(
       JSON.stringify({
-        username: username,
-        text: text,
+        username: username.value,
+        text: text.value,
       })
     );
-    $("#input-text")[0].value = "";
+    text.value = "";
   });
- });
+});
