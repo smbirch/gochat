408eaf388f7b530893df0b147f9eb6c6dd5d780b
smbirch <spencer.m.birch@gmail.com>
2022-02-11 11:02:07 -0800

    adding websockets

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/gorilla/websocket v1.4.2
 	github.com/joho/godotenv v1.4.0
 )
 
--- public/app.js
+++ public/app.js
@@ -1,9 +1,22 @@
-window.addEventListener('DOMContentLoaded', (_) => {
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        let username = document.getElementById("input-username");
-        username.value = "";
-        let text = document.getElementById("input-text");
-        text.value = "";
-      });
-     });
+$(function () {
+  let websocket = newWebSocket("wss://" + window.location.host + "/websocket");
+  let room = $("#chat-text");
+  websocket.addEventListener("message", function (e) {
+    let data = JSON.parse(e.data);
+    let chatContent = `<p><strong>${data.username}</strong>: ${data.text}</p>`;
+    room.append(chatContent);
+    room.scrollTop = room.scrollHeight; // Auto scroll to the bottom
+   });
+  $("#input-form").on("submit", function (event) {
+    event.preventDefault();
+    let username = $("#input-username")[0].value;
+    let text = $("#input-text")[0].value;
+    websocket.send(
+      JSON.stringify({
+        username: username,
+        text: text,
+      })
+    );
+    $("#input-text")[0].value = "";
+  });
+ });
